62bd08d94bb3f186d6b915dc6a0b9bcc3ef868b4
Rafael <vitorino.rafael55@gmail.com>
2023-03-03 13:41:42 -0300

    feat: initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { ContractService } from './contract.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angular-metamask';
+
+
+  constructor(
+    private readonly _contractService: ContractService
+  ) { }
+
+  public connect() {
+    this._contractService.openMetamask()
+  }
 }
